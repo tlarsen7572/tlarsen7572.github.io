52598fa79b0d62be13b2346aca1f0562c5e3a301
Tom Larsen <larsenthomasj@gmail.com>
2021-02-07 20:46:25 -0500

    try changing image location

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,20 +4,20 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4b65d2b4b36671b8c3a4432eccf87272",
-"index.html": "32c0dff29e5290b641a602bf5a6ae43e",
-"/": "32c0dff29e5290b641a602bf5a6ae43e",
-"main.dart.js": "9e2ef33ac5ee3283f1a95ac5ee6a192c",
+"index.html": "aa95592c358f7bf5de6c2acd56c82bb7",
+"/": "aa95592c358f7bf5de6c2acd56c82bb7",
+"main.dart.js": "af039df3ed798dca60fc367659559642",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "5b147e049783c556328c618ad77f07ee",
-"assets/AssetManifest.json": "a722e7196fdfe9cc4af535f1e1240b1d",
+"assets/images/Tom.jpeg": "f6dc0c461db44fc0886911f2cbe42f43",
+"assets/images/goalteryx_icon.png": "97820c0f221fca4d07121422d59195b3",
+"assets/AssetManifest.json": "00684a3d2152bc33e94cf262d73cd549",
 "assets/NOTICES": "7f3f6999de5a088b0510c489ac5660d9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/assets/Tom.jpeg": "f6dc0c461db44fc0886911f2cbe42f43",
-"assets/assets/goalteryx_icon.png": "97820c0f221fca4d07121422d59195b3"
+"assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac"
 };
 
 // The application shell files that are downloaded before a service worker can
